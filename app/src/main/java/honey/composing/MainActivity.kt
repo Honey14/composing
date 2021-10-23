dbef33bdd4a7b3aa7f25d8cac1d154835a97932b
honeysonwani <honey@obvious.in>
2021-10-23 18:10:06 +0530

    Use theme attributes and text properties

--- app/src/main/java/honey/composing/MainActivity.kt
+++ app/src/main/java/honey/composing/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
@@ -13,6 +14,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.primarySurface
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import honey.composing.ui.theme.ComposingTheme
@@ -45,11 +49,17 @@ private fun Menu(foodItem: FoodItems) {
             painter = painterResource(foodItem.resourceId),
             contentDescription = "Egg Sandwich",
             modifier = Modifier
-                .size(36.dp)
+                .size(89.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, Color.LightGray, CircleShape)
         )
-        Text(text = foodItem.foodName)
         Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = foodItem.foodName,
+            color = Color.Black,
+            style = MaterialTheme.typography.subtitle2,
+            modifier = Modifier.padding(all = 4.dp)
+        )
     }
 }
 
