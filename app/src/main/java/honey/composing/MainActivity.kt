1b17db0bee69c21e914c9cd7271d5de2c44d5883
honeysonwani <honey@obvious.in>
2021-10-23 15:52:19 +0530

    Wrap the image and text inside the Row

--- app/src/main/java/honey/composing/MainActivity.kt
+++ app/src/main/java/honey/composing/MainActivity.kt
@@ -3,12 +3,15 @@ package honey.composing
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import honey.composing.ui.theme.ComposingTheme
 
@@ -20,6 +23,12 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-private fun foodItems() {
-    Text(text = "Egg sandwich")
+private fun foodItem() {
+    Row {
+        Image(
+            painter = painterResource(R.drawable.eggsand),
+            contentDescription = "Egg Sandwich"
+        )
+        Text(text = "Egg sandwich")
+    }
 }
