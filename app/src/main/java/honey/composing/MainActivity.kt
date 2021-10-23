09ed3e1934c8dae44fdf48786d204648fc715c66
honeysonwani <honey@obvious.in>
2021-10-23 17:11:17 +0530

    Create `ListOfMenuItems` composable function

--- app/src/main/java/honey/composing/MainActivity.kt
+++ app/src/main/java/honey/composing/MainActivity.kt
@@ -2,22 +2,17 @@ package honey.composing
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.tooling.preview.Preview
-import honey.composing.ui.theme.ComposingTheme
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -40,5 +35,14 @@ private fun foodItem() {
         )
         Text(text = "Egg sandwich")
         Spacer(modifier = Modifier.width(8.dp))
+    }
+}
+
+@Composable
+fun ListOfMenuItems(foodItems: List<FoodItems>) {
+    LazyColumn {
+        items(foodItems) { foodItem ->
+            Menu(foodItem)
+        }
     }
 }
